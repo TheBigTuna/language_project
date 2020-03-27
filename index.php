84444113b7b432986c3a60c9fd2fba52d7522154
octavius <mooreoctavius94@gmail.com>
2020-03-27 14:49:59 -0400

    update 3/28/2020
    update 3/28/2020

--- index.php
+++ index.php
@@ -3,26 +3,29 @@
 ?>
 <link href="/style/index.css" rel="stylesheet">
 
-<div id="videoBG" class="d-none d-md-block">
-    <div id="headWrapper">
-        <h2 id="headText">Learn Code Anywhere</h2>
-    </div>
-    <video autoplay loop muted class="wrapper__video">
-        <source src="video/headerVideo.mp4">
-    </video>
-</div>
-
-<div class="row">
-    <div class="col-0 col-sm-3"></div>
-    <div class="col-12 col-sm-6">
-        <div id="lessons">
-            <ul class="list-group">
-            <h2 id="lessonsText">Courses Available</h2>
-            <a href="lessons.php?Type=HTML"><li class="list-group-item"><h4 class="text-center">HTML</h4></li></a>
-            <a href="lessons.php?Type=CSS"><li class="list-group-item"><h4 class="text-center">CSS</h4></li></a>
-            </ul>
+<div id="homeContainer">
+    <div id="videoBG" class="d-none d-md-block">
+        <div id="headWrapper">
+            <h2 id="headText">GrowthBook</h2>
+            <h2 id="subText">Learn Code Anywhere</h2>
         </div>
+        <video autoplay loop muted class="wrapper__video">
+            <source src="video/headerVideo.mp4">
+        </video>
     </div>
-    <div class="col-0 col-sm-3"></div>
+
+    <div class="row">
+        <div class="col-0 col-sm-3"></div>
+        <div class="col-12 col-sm-6">
+            <div id="lessons">
+                <ul class="list-group">
+                <h2 id="lessonsText">Courses Available</h2>
+                <a href="lessons.php?Type=HTML"><li class="list-group-item"><h4 class="text-center">HTML</h4></li></a>
+                <a href="lessons.php?Type=CSS"><li class="list-group-item"><h4 class="text-center">CSS</h4></li></a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-0 col-sm-3"></div>
+    </div>
 </div>
 
