7d649696c4f7a338b513506589fff67d9f81cef3
octavius <mooreoctavius94@gmail.com>
2020-05-10 16:27:50 -0400

    update 5/10/2020
    update 5/10/2020

--- navbar.php
+++ navbar.php
@@ -2,17 +2,15 @@
 <!DOCTYPE html>
     <html lang="en">
         <head>
-          <?php
-              include("resources/connection.php"); 
-          ?>
+          	  <?php include("resources/connection.php"); ?>
               <!-- <title><?= $_SESSION['CurrentPage']; ?></title> -->
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1">
               <!-- <meta name='description' content="<?= $_SESSION['Description']; ?>">             -->
               <!-- <meta name="author" content="<?= $_SESSION['ArticleAuthor']; ?>"> -->
               <!-- <link rel="icon" href="/images/halfLogo.png"> -->
-              <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
               <link href="/style/bootstrap.css" rel="stylesheet">
+			  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
               <link href="/style/font-awesome.min.css" rel="stylesheet">
               <link href="/style/animate.css" rel="stylesheet">
               <link href="/style/main.css" rel="stylesheet">
