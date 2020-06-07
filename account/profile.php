0cf49ca82d60080133a6746a01a9d74d9931c1dc
octavius <mooreoctavius94@gmail.com>
2020-06-06 22:05:13 -0400

    update 6/6/2020
    update 6/6/2020

--- account/profile.php
+++ account/profile.php
@@ -1,6 +1,7 @@
 <?php
  include('../navbar.php');
 ?>
+<link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
 <link href="/style/profile.css" rel="stylesheet">
 
 <div id="profileContainer">
@@ -9,14 +10,52 @@
         <div class="col-12 col-lg-7">
             <div id="userInfoBG">
                 <div id="displayUser">
-                    test
+                    <div class="row">
+                        <div class="col-12 col-lg-3"></div>
+                        <div class="col-12 col-lg-9">
+                            <div class="mt-5">
+                                <h4 id="displayUserName"><?= $_SESSION['User']; ?></h4>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="mt-5">
+                    <?php
+                        for($i = 0; $i < count($_SESSION['UserCourses']); $i++){
+                            switch ($_SESSION['UserCourses'][$i]){
+                                case "HTML5":
+                                    $Icon = "<i class='devicon-html5-plain-wordmark colored'></i>";    
+                                break;
+                                case "CSS3":
+                                    $Icon = "<i class='devicon-css3-plain-wordmark colored'></i>";
+                                break;
+                            }
+                    ?>
+                    <a href="/lessons.php?Type=<?= $_SESSION['UserCourses'][$i]; ?>">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col-12">
+                                    <?= $Icon; ?> 
+                                    
+                                </div>
+                            </div>
+                            <div class="answer"></div>
+                        </div>
+                    </a>
+                    <?php
+                        }
+                    ?>
+                </div>
+
+
             </div>
         </div>
         <div class="col-12 col-lg-3">
             <div id="profileSidebar">
                 <ul class="list-group list-unstyled">
-                    <li><a class="list-group-item" href="#">User Info</a>
+                    <li><a class="list-group-item" href="profile.php">Account</a>
+                    <li><a class="list-group-item" href="userInfo.php">User Info</a>
                     <li><a class="list-group-item" href="#">Email Notifications</a>
                     <li><a class="list-group-item" href="#">Privacy Settings</a>
                     <li><a class="list-group-item" href="#">Subscription</a>
@@ -27,3 +66,12 @@
         <div class="col-0 col-lg-1"></div>
     </div>
 </div>
+<div class="row">
+    <div class="col-0 col-lg-1"></div>
+    <div class="col-0 col-lg-10">
+
+    
+    </div>
+    <div class="col-0 col-lg-1">
+</div>
+
---
+++ account/userInfo.php
@@ -0,0 +1,103 @@
+<?php
+ include('../navbar.php');
+?>
+<link href="/style/profile.css" rel="stylesheet">
+
+<div id="profileContainer">
+    <div class="row">
+        <div class="col-0 col-lg-1"></div>
+        <div class="col-12 col-lg-7">
+            <div id="userInfoBG">
+                <div id="displayUser">
+
+
+
+
+
+
+                
+      <form action="index.html" method="post">
+      <h1>Sign Up</h1>
+      <fieldset>
+        <legend><span class="number">1</span>Your basic info</legend>
+        <label for="name">First Name:</label>
+        <input type="text" id="name" name="firstName">
+
+        <label for="name">Last Name:</label>
+        <input type="text" id="name" name="lastName">
+        
+        <label for="mail">Email:</label>
+        <input type="email" id="mail" name="user_email">
+        
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="user_password">
+        
+        <label>Age:</label>
+        <input type="radio" id="under_13" value="under_13" name="user_age"><label for="under_13" class="light">Under 13</label><br>
+        <input type="radio" id="over_13" value="over_13" name="user_age"><label for="over_13" class="light">13 or older</label>
+      </fieldset>
+      
+      <fieldset>
+        <legend><span class="number">2</span>Your profile</legend>
+        <label for="bio">Biography:</label>
+        <textarea id="bio" name="user_bio"></textarea>
+      </fieldset>
+      <fieldset>
+      <label for="job">Job Role:</label>
+      <select id="job" name="user_job">
+        <optgroup label="Web">
+          <option value="frontend_developer">Front-End Developer</option>
+          <option value="php_developor">PHP Developer</option>
+          <option value="python_developer">Python Developer</option>
+          <option value="rails_developer"> Rails Developer</option>
+          <option value="web_designer">Web Designer</option>
+          <option value="WordPress_developer">WordPress Developer</option>
+        </optgroup>
+        <optgroup label="Mobile">
+          <option value="Android_developer">Androild Developer</option>
+          <option value="iOS_developer">iOS Developer</option>
+          <option value="mobile_designer">Mobile Designer</option>
+        </optgroup>
+        <optgroup label="Business">
+          <option value="business_owner">Business Owner</option>
+          <option value="freelancer">Freelancer</option>
+        </optgroup>
+        <optgroup label="Other">
+          <option value="secretary">Secretary</option>
+          <option value="maintenance">Maintenance</option>
+        </optgroup>
+      </select>
+      
+        <label>Interests:</label>
+        <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="light" for="development">Development</label><br>
+          <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="light" for="design">Design</label><br>
+        <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="light" for="business">Business</label>
+      
+      </fieldset>
+      <button type="submit">Edit Account</button>
+    </form>
+
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-3">
+            <div id="profileSidebar">
+                <ul class="list-group list-unstyled">
+                    <li><a class="list-group-item" href="profile.php">Account</a>
+                    <li><a class="list-group-item" href="#">User Info</a>
+                    <li><a class="list-group-item" href="#">Email Notifications</a>
+                    <li><a class="list-group-item" href="#">Privacy Settings</a>
+                    <li><a class="list-group-item" href="#">Subscription</a>
+                    <li><a class="list-group-item" href="#">Purchase History</a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-0 col-lg-1"></div>
+    </div>
+</div>
